b4b1ffbf9eec613d373936b26b323dd6baabee4d
Michael Mburu <ichangaimichael@yahoo.com>
2022-09-10 20:18:13 +0300

    - Added Generic Entity

--- src/Components/Genres/EditGenre.tsx
+++ src/Components/Genres/EditGenre.tsx
@@ -1,45 +1,22 @@
-import axios, { AxiosResponse } from 'axios'
-import React, { useEffect, useState } from 'react'
-import {useNavigate, useParams} from 'react-router-dom'
 import { urlGenres } from '../../Endpoints'
-import DisplayErrors from '../../Utils/DisplayErrors'
-import Loading from '../../Utils/Loading'
+import EditEntity from '../../Utils/EditEntity'
 import GenreForm from './GenreForm'
 import { GenreCreationDTO, GenreDTO } from './genres.model'
 const EditGenre = () => {
-
-  const {id}: any = useParams()
-  const [genre, setGenre] = useState<GenreCreationDTO>();
-  const [errors, setErrors] = useState<string[]>([])
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    axios.get(`${urlGenres}/${id}`)
-          .then((response: AxiosResponse<GenreDTO>) => {
-            setGenre(response.data)
-          })
-  }, [id])
-
-  const edit = async (genreToEdit: GenreCreationDTO) => {
-    try{
-      await axios.put(`${urlGenres}/${id}`, genreToEdit)
-    } catch(error: any){
-      if(error && error.response)
-      {
-        setErrors(error.response.data)
-      }
-      
-    }
-  }
+  
   return (
     <>
-        <h3>Edit Genre</h3>
-        <DisplayErrors errors={errors} />
-          {genre &&  <GenreForm model={genre} onSubmit = { async value =>{
-              await edit(value)
-              navigate("/genres")
-          }}
-       /> }
+       <EditEntity<GenreCreationDTO, GenreDTO>
+          Url={urlGenres} entityName="Genres" indexUrl='/genres'
+        >
+        {(entity, edit) => 
+          <GenreForm model={entity} onSubmit = { async value =>{
+            await edit(value)
+
+        }}
+     /> 
+        }
+       </EditEntity>
     </>
   )
 }
---
+++ src/Utils/EditEntity.tsx
@@ -0,0 +1,53 @@
+import axios, { AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react'
+import { ReactElement } from 'react-markdown/lib/react-markdown';
+import { useNavigate, useParams } from 'react-router-dom';
+import DisplayErrors from './DisplayErrors';
+
+export default function EditEntity<TCreation, TRead>(props: EditEntityProps<TCreation, TRead>) {
+
+  const {id}: any = useParams()
+  const [entity, setEntity] = useState<TCreation>();
+  const [errors, setErrors] = useState<string[]>([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios.get(`${props.Url}/${id}`)
+          .then((response: AxiosResponse<TRead>) => {
+            setEntity(props.transform(response.data))
+          })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const edit = async (entityToEdit: TCreation) => {
+    try{
+      await axios.put(`${props.Url}/${id}`, entityToEdit)
+      navigate(props.indexUrl)
+    } catch(error: any){
+      if(error && error.response)
+      {
+        setErrors(error.response.data)
+      }
+      
+    }
+  }
+  return (
+    <div>
+         <h3>Edit {props.entityName}</h3>
+          <DisplayErrors errors={errors} />
+          {entity &&  props.children(entity, edit)}
+    </div>
+  )
+}
+
+interface EditEntityProps<TCreation, TRead> {
+  Url: string
+  entityName: string
+  indexUrl: string
+  children(entity: TCreation, edit:  (entity: TCreation) => void): ReactElement
+  transform(entity: TRead): TCreation
+}
+
+EditEntity.defaultProps = {
+  transform: (entity: any) => entity
+}
